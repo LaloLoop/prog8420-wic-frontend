9bfef93bf54513579307a217e3a174e997d476f4
Mdiemand7248@conestogac.on.ca <Mdiemand7248@conestogac.on.ca>
2022-04-10 19:29:43 +0000

    Edited views.person.Update_Person

--- client_code/views/person/Update_Person/__init__.py
+++ client_code/views/person/Update_Person/__init__.py
@@ -7,10 +7,10 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class Update_Person(Update_PersonTemplate):
-  def __init__(self, **properties):
+  def __init__(self, router=None, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    self.router = router
     # Any code you write here will run when the form opens.
 
     # use GET request to programmatically make form from JSON, or setup manually?
