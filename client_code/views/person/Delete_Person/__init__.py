393efe796e253f607d47d0a45e095c04f5ba8d04
Mdiemand7248@conestogac.on.ca <Mdiemand7248@conestogac.on.ca>
2022-04-11 00:06:51 +0000

    Created views.patient.CRUD_Patient, views.patient.Create_Patient, views.employee.CRUD_Employee, views.job.Update_Job, views.appointment.Update_Appointment, views.employee.Create_Employee, views.appointment.Delete_Appointment, views.employee.Delete_Employee, views.appointment.Create_Appointment, views.unit.CRUD_Unit, views.patient.Delete_Patient, views.unit.Create_Unit, views.appointment.CRUD_Appointment, views.employee.Update_Employee, views.prescription.CRUD_Prescription, views.appointment.Read_Appointment, views.job.CRUD_Job, views.job.Read_Job, views.unit.Delete_Unit, views.job.Delete_Job, views.prescription.Update_Prescription, views.prescription.Read_Prescription, views.prescription.Delete_Prescription, views.prescription.Create_Prescription, views.patient.Read_Patient, views.patient.Update_Patient, views.employee.Read_Employee, views.unit.Read_Unit and views.unit.Update_Unit; Edited views.person.Update_Person, views.person.Create_Person, views.person.Delete_Person, views.person.Read_Person, views.job.Create_Job and views.person.CRUD_Person

--- client_code/views/person/Delete_Person/__init__.py
+++ client_code/views/person/Delete_Person/__init__.py
@@ -6,6 +6,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+model_name = 'person'
+
 class Delete_Person(Delete_PersonTemplate):
   def __init__(self, router=None, **properties):
     # Set Form properties and Data Bindings.
@@ -18,14 +20,14 @@ class Delete_Person(Delete_PersonTemplate):
     # Any code you write here will run when the form opens.
 
   def button_back_click(self, **event_args):
-    self.router.nav_to_route_view(self, 'person', 'crud')
+    self.router.nav_to_route_view(self, model_name, 'crud')
   
   def button_submit_click(self, **event_args):
     # use DELETE request to web api  
     
     # after successful submission,
     # redirect back to CRUD_Home
-    self.router.nav_to_route_view(self, 'person', 'crud')
+    self.router.nav_to_route_view(self, model_name, 'crud')
 
 
 
--- client_code/views/person/Read_Person/__init__.py
+++ client_code/views/person/Read_Person/__init__.py
@@ -6,17 +6,25 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-model = 'person' # change this for different entities
+model_name = 'person'
 
 class Read_Person(Read_PersonTemplate):
   def __init__(self, router=None, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.router = router
+
+    self.item['label_id_value'] = 'test'
+
+    self.refresh_data_bindings()
+
     # Any code you write here will run when the form opens.
-    
-    # use GET request to programmatically make form from JSON, or setup manually?
 
   def button_back_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    self.router.nav_to_route_view(self, 'person', 'crud')
+    self.router.nav_to_route_view(self, model_name, 'crud')
+
+  def button_submit_click(self, **event_args):
+    # use DELETE request to web api
+
+    # after successful submission,
+    # redirect back to CRUD_Home
+    self.router.nav_to_route_view(self, model_name, 'crud')
