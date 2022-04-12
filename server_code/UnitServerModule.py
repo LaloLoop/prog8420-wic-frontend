9c54771706cd15555a8f5c98036c098e20a3c5f1
Mdiemand7248@conestogac.on.ca <Mdiemand7248@conestogac.on.ca>
2022-04-12 23:00:20 +0000

    Created UnitServerModule; Deleted ServerModule1

---
+++ server_code/UnitServerModule.py
@@ -0,0 +1,24 @@
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+@anvil.server.callable
+def set_units(units):
+  anvil.server.session["session_units"] = units
+  
+@anvil.server.callable
+def get_units():
+  return anvil.server.session.get("session_units")
+  
+@anvil.server.callable
+def set_selected_unit_id(unit_id):
+  anvil.server.session["selected_unit_id"] = unit_id
+
+@anvil.server.callable
+def get_selected_unit():
+  set_selected_unit_id = anvil.server.session.get("selected_unit_id")
+  session_units = anvil.server.session.get("session_units")
+  selected_unit = next(x for x in session_units if x["id"] == set_selected_unit_id)
+  return selected_unit
