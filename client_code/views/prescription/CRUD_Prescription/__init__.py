b1ffa173deba0c4db77e1e526793d782fefd2ac0
Mdiemand7248@conestogac.on.ca <Mdiemand7248@conestogac.on.ca>
2022-04-13 02:51:26 +0000

    Edited UnitServerModule, views.prescription.CRUD_Prescription and views.prescription.Create_Prescription

--- client_code/views/prescription/CRUD_Prescription/__init__.py
+++ client_code/views/prescription/CRUD_Prescription/__init__.py
@@ -34,7 +34,6 @@ class CRUD_Prescription(CRUD_PrescriptionTemplate):
     url = f'{self.router.base_url}{model_name}s_with_id_display_name'
     resp = anvil.http.request(url, method='GET', json=True)
     self.repeating_panel_1.items = resp
-    print(resp)
     anvil.server.call('set_prescriptions', resp)
     #list_of_display_name_tuples = [(e['name'], e['id']) for e in resp]
     #self.drop_down_all_entities.items = list_of_display_name_tuples
