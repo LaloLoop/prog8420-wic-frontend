dc7354f255fc281c84e854fb6bfd56dc3d4df5dc
Mdiemand7248@conestogac.on.ca <Mdiemand7248@conestogac.on.ca>
2022-04-15 17:26:59 +0000

    Edited views.unit.Read_Unit

--- client_code/views/unit/Read_Unit/__init__.py
+++ client_code/views/unit/Read_Unit/__init__.py
@@ -19,8 +19,6 @@ class Read_Unit(Read_UnitTemplate):
     self.router.nav_to_route_view(self, model_name, 'crud')
     
   def form_show(self, **event_args):
-    """This method is called when the column panel is shown on the screen"""
-    pass
     _id = anvil.server.call('get_selected_entity_id')
     url = f"{self.router.base_url}{model_name}/{_id}"
     resp = anvil.http.request(url, method='GET', json=True)
