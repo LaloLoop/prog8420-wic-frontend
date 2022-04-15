7941879bc592c27ea85a03279c379a1d66fe5971
gt.silva.e@gmail.com <gt.silva.e@gmail.com>
2022-04-15 23:41:33 +0000

    Login attempt with cookies

--- client_code/views/auth/LoginForm/__init__.py
+++ client_code/views/auth/LoginForm/__init__.py
@@ -20,5 +20,29 @@ class LoginForm(LoginFormTemplate):
     user = self.user_box.text
     password = self.password_box.text
     
+    url = f"{self.router.base_url}auth/jwt/login"
+    data_dict = {'username': user, 'password': password}
     
-
+    self.feedback_banner.visible = False
+    
+    user_data = None
+    try:
+      anvil.http.request(url, method="POST", data=data_dict)
+      user_data = anvil.http.request(f"{self.router.base_url}users/me", json=True)
+      anvil.server.call("store_autenticated", user_data)
+      banner_color = '#a5d6a7'
+      feedback = "Login successful"
+      
+    except anvil.http.HttpError as e:
+      banner_color = '#ff5252'
+      if e.status == 500:
+        feedback = f"({e.status}) We had a problem processing your request"
+      else:
+        feedback = f"({e.status}) Please check your credentials"
+        
+    self.feedback_banner.text = feedback
+    self.feedback_banner.visible = True  
+    self.feedback_banner.background = banner_color
+    
+    if user_data is not None:
+      self.router.nav_to_route_view(self, 'home', 'admin')
