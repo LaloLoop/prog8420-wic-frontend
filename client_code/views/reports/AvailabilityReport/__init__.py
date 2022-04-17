4a6f8b1dee4da88b180faaac41afde5953b33a64
gt.silva.e@gmail.com <gt.silva.e@gmail.com>
2022-04-17 22:28:11 +0000

    Working scrollbar for availability report

--- client_code/views/reports/AvailabilityReport/__init__.py
+++ client_code/views/reports/AvailabilityReport/__init__.py
@@ -22,18 +22,19 @@ class AvailabilityReport(AvailabilityReportTemplate):
     
     columns = data['columns']
     report_data = data['data']
-    col_width = 200
+    first_col_width = 200
+    col_width = 100
     
     column_names = ['Doctor'] + [s[s.index('T') + 1:-3] for s in columns[1:]]
     
     grid_cols=[{'id':col,
-                'width':col_width, 
+                'width': first_col_width if i == 0 else col_width, 
                 'title':title,
-                'data_key':col} for title, col in zip(column_names, columns)]
+                'data_key':col} for i, (title, col) in enumerate(zip(column_names, columns))]
     
-    self.av_grid.rows_per_page = 3
+    self.av_grid.rows_per_page = 10
     self.av_grid.show_page_controls = True
-    self.av_grid.width = col_width * len(grid_cols)
+    #self.av_grid.width = col_width * len(grid_cols)
     self.av_grid.columns = grid_cols
     self.repeating_panel_1.items = report_data
     
@@ -41,6 +42,11 @@ class AvailabilityReport(AvailabilityReportTemplate):
   def nav_back_click(self, **event_args):
     """This method is called when the button is clicked"""
     self.router.nav_to_route_view(self, 'home', 'admin')
+
+  def av_grid_show(self, **event_args):
+    """This method is called when the data grid is shown on the screen"""
+    self.gen_report_click()
+
 
 
 
