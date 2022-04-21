784dd76fe8c31dfebc938adbf5c8ed85a72b309e
Mdiemand7248@conestogac.on.ca <Mdiemand7248@conestogac.on.ca>
2022-04-21 20:54:00 +0000

    Edited views.reports.TimeslotUsageReport

--- client_code/views/reports/TimeslotUsageReport/__init__.py
+++ client_code/views/reports/TimeslotUsageReport/__init__.py
@@ -11,7 +11,7 @@ class TimeslotUsageReport(TimeslotUsageReportTemplate):
   def __init__(self, router, **properties):
     self.init_components(**properties)
     self.router = router
-    self.av_grid.role = 'wide'
+    self.tsu_grid.role = 'wide'
 
   def gen_report_click(self, **event_args):
     data = anvil.http.request(f"{self.router.base_url}reports/timeslot-usage", json=True)
@@ -28,21 +28,21 @@ class TimeslotUsageReport(TimeslotUsageReportTemplate):
                 'title':title,
                 'data_key':col} for i, (title, col) in enumerate(zip(column_names, columns))]
 
-    self.av_grid.rows_per_page = 10
-    self.av_grid.show_page_controls = True
-    #self.av_grid.width = col_width * len(grid_cols)
-    self.av_grid.columns = grid_cols
+    self.tsu_grid.rows_per_page = 10
+    self.tsu_grid.show_page_controls = True
+    #self.tsu_grid.width = col_width * len(grid_cols)
+    self.tsu_grid.columns = grid_cols
     self.repeating_panel_1.items = report_data
 
-    self.get_av_graph()
+    self.get_tsu_graph()
 
-  def get_av_graph(self):
-    self.av_graph.source = f"{self.router.base_url}graphs/timeslot-usage"
+  def get_tsu_graph(self):
+    self.tsu_graph.source = f"{self.router.base_url}graphs/timeslot-usage"
 
   def nav_back_click(self, **event_args):
     self.router.nav_to_route_view(self, 'home', '')
 
-  def av_grid_show(self, **event_args):
+  def tsu_grid_show(self, **event_args):
     self.gen_report_click()
 
   def button_availability_report_click(self, **event_args):
@@ -56,3 +56,5 @@ class TimeslotUsageReport(TimeslotUsageReportTemplate):
 
   def button_entity_count_click(self, **event_args):
     self.router.nav_to_route_view(self, 'report', 'entity_count')
+
+
